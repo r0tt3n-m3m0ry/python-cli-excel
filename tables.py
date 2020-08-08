a01faef2e6d929a1d29e79f89b0e43dfe37eb091
Oleg Voevodin <r0tt3n-m3m0ry@protonmail.com>
2020-08-08 03:53:59 +0300

    Suck my di

--- tables.py
+++ tables.py
@@ -6,6 +6,7 @@ except ImportError:
     print('Install requirement modules via \'$ pip3 install -r requirements.txt\''); exit()
 
 import os
+import random
 
 def cls():
     os.system('cls') if os.name == 'nt' else os.system('clear')
@@ -13,7 +14,7 @@ def cls():
 def saving_table():
     pass
 
-def opening_table():
+def loading_table():
     pass
 
 table = PrettyTable()
@@ -39,8 +40,8 @@ except:
     print('\nAll column titles must be unique!\n'); exit()
 
 while True:
-    for value in range(count_of_fields):
-        row_values.append(input('\nEnter next value: '))
+    for value in fields:
+        row_values.append(input(f'\nEnter value for {value}: '))
     table.add_row(row_values)
     row_values = []
     
@@ -54,20 +55,30 @@ while True:
     if choice[0].lower() == 'e':
         cls()
         print(table)
+        table_html = table.get_html_string()
+        table_title=input('\nEnter table title: ')
         filename = input('\nEnter name of file and extention, splitted by dot (for example, \'example.txt\', if not extention - default .txt): ')
+        file_name, file_ext = filename.split('.')[0], filename.split('.')[-1]
         if os.path.exists(filename) == True:
-            filename = filename + f'_{filename[-1]}'
+            if input('This file already exists. Type \'rewrite\' for re-write the file or something for magic: ').lower() != 'rewrite':
+                filename = f'{file_name}_{random.randint(0, 9)}.{file_ext}'
         if len(filename.split('.')) != 1:
             table_file = open(filename, 'w')
-            table_file.write(str(table) + '\n\n')
+            table_file.write(table.get_string(title=table_title) + '\n\n')
             table_file.close()
+            table_html_file = open(filename.split('.')[0] + '.html', 'w')
+            table_html_file.write(str(table_html) + '\n\n')
+            table_html_file.close()
         else:
             table_file = open(filename + '.txt', 'w')
-            table_file.write(str(table) + '\n\n')
+            table_file.write(table.get_string(title=table_title) + '\n\n')
             table_file.close()
+            table_html_file = open(filename + '.html', 'w')
+            table_html_file.write(str(table_html) + '\n\n')
+            table_html_file.close()
         break
     elif choice[0].lower() == 'n':
         continue
     else:
-        print('\nFuck you, stupid user!\n')
+        print('\nHey you, cocksucker! Read my ass instruction before penetrating!\n')
         break
